3b3968e318f3ec2b964d8c7c5a5a4ac0924ce1f1
Gaurav Jain <gaurav.jain2@soprabanking.com>
2021-01-20 09:22:55 +0530

    add file changed

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print("Hello")
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+@app.route('/how are you')
+def hello():
+    return 'Hello, I am fine and you?')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
